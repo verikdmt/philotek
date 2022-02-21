65f333d73ee8a66613d468c4ab1ad4d5f4905802
verikdmt <84927975+verikdmt@users.noreply.github.com>
2022-02-21 17:01:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,3 +22,25 @@ window.onscroll = function(){
            header.classList.remove('active');
        }
     }
+
+
+$(document).ready(function() {
+
+    //E-mail Ajax Send
+    $("form").submit(function() {
+        var th = $(this);
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: th.serialize()
+        }).done(function() {
+            alert("Thank you!");
+            setTimeout(function() {
+                // Done Functions
+                th.trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+});
